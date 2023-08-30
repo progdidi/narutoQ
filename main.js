00627065f11ba2047f2c2fe2f7d05e5907fd7a67
Diana Sosnovskaya <progdidi@gmail.com>
2023-08-30 16:38:08 +0200

    change style

--- main.js
+++ main.js
@@ -1,31 +1,29 @@
 const quotes = [
     {
         text: 'Быть идеальным тоже непросто. Если ты сильный, ты очень скоро остаёшься один и становишься заносчивым, даже если изначально ты давал людям всё, чего от тебя ожидали.',
+        source: 'Наруто',
         id: 1,
         episode: 12
     }, 
 
     {
         text: 'У того, кто свернёт с пути справедливости, нет смелости, но когда есть сильный лидер, трусости не остаётся места. Так сказал первый Хокаге.',
+        source: 'Наруто',
         id: 2,
         episode: 12
     },
 
     {
         text: 'Не важно, что за тьма ждет впереди. Я не сойду с этого пути! Я обрету силу, несмотря ни на что!',
-        id: 2,
+        id: 3,
+        source: 'Наруто',
         episode: 12
     },
 
     {
         text: 'За моею спиной крылья, имя которым настойчивость! Порою настойчивость может принять форму крыльев и сделать даже невозможное возможным!',
-        id: 2,
-        episode: 12
-    },
-
-    {
-        text: 'pupupu4',
-        id: 2,
+        source: 'Наруто',
+        id: 4,
         episode: 12
     }
 
@@ -33,12 +31,15 @@ const quotes = [
 ]
 
 const quote = document.querySelector('.quotes__content-text');
+const source = document.querySelector('.quotes__content-source');
 const quoteBtn = document.querySelector('.quotes__content-btn');
 
 quote.innerHTML = quotes[0].text;
+source.innerHTML = quotes[0].source;
 
 quoteBtn.addEventListener('click', () => {
     let random = Math.floor(Math.random() * 10);
     quote.innerHTML = quotes[random].text;
+    source.innerHTML = quotes[random].source;
     quote.classList.add('animated')
 })
